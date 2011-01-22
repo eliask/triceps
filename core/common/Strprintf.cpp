98593c9879c917592b87d3e0af4945542ad0f168
babkin <babkin@b66b675a-a689-4d1e-b9d0-6c48a4198705>
2011-01-22 04:45:28 +0000

    A simple infrastructure to run the C++ unit tests.

--- core/common/Strprintf.cpp
+++ core/common/Strprintf.cpp
@@ -15,15 +15,18 @@ string strprintf(const char *fmt, ...)
 {
 	char buf[500];
 	va_list ap;
-	va_start(ap, fmt);
 
+	va_start(ap, fmt);
 	int n = vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
 	if (n < sizeof(buf))
 		return string(buf);
 
 	// a more complicated case, with a large string
 	char *s = new char[n+1];
+	va_start(ap, fmt);
 	vsnprintf(s, n+1, fmt, ap);
+	va_end(ap);
 	string ret(s);
 	delete[] s;
 	return ret;
---
+++ core/common/test/t_strprintf.cpp
@@ -0,0 +1,35 @@
+//
+// This file is a part of Biceps.
+// See the file COPYRIGHT for the copyright notice and license information
+//
+//
+// Test of strprintf().
+
+#include <stdio.h>
+#include <common/Strprintf.h>
+
+using namespace Biceps;
+
+bool eCode = 0;
+
+void fail()
+{
+	eCode = 1;
+}
+
+// returns the shell error code
+int getCode()
+{
+	return eCode;
+}
+
+int main()
+{
+	std::string a = strprintf("%s", "aa");
+	std::string b = strprintf("%1000s", "bc");
+
+	if (a.size() !=2 || b.size() != 1000)
+		fail();
+
+	return getCode();
+}
