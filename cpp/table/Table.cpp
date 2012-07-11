52005a5f17332b2841a520ce3c8918a24dfa1f81
babkin <babkin@b66b675a-a689-4d1e-b9d0-6c48a4198705>
2012-07-11 02:08:39 +0000

    Fixed a mistake in copy-paste.

--- cpp/table/Table.cpp
+++ cpp/table/Table.cpp
@@ -399,7 +399,7 @@ size_t Table::groupSizeIdx(IndexType *ixt, const RowHandle *what) const
 size_t Table::groupSizeRowIdx(IndexType *ixt, const Row *row) const
 {
 	if (row == NULL)
-		return NULL;
+		0;
 
 	RowHandle *rh = makeRowHandle(row);
 	rh->incref();
