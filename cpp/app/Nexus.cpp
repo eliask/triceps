ca53601ccccfb00447e79d960dc9a1b103ec8cbc
babkin <babkin@b66b675a-a689-4d1e-b9d0-6c48a4198705>
2013-02-05 02:09:56 +0000

    The basic implementation of Nexus methods.

--- cpp/app/Nexus.cpp
+++ cpp/app/Nexus.cpp
@@ -15,7 +15,78 @@ Nexus::Nexus(const string &name) :
 	name_(name),
 	reverse_(false),
 	unicast_(false)
+{ }
+
+Nexus *Nexus::setReverse(bool on)
 {
+	assertNotExported();
+	reverse_ = on;
+	return this;
+}
+
+Nexus *Nexus::setUnicast(bool on)
+{
+	assertNotExported();
+	unicast_ = on;
+	return this;
+}
+
+Nexus *Nexus::setType(Onceref<RowSetType> rst)
+{
+	assertNotExported();
+	Erref err = rst->getErrors();
+	if (err->hasError()) {
+		errefAppend(err_, "The row set type contains an error:", err);
+		return this;
+	}
+	type_ = rst->deepCopy();
+	return this;
+}
+
+Nexus *Nexus::addRow(const string &rname, const_Autoref<RowType>rtype)
+{
+	assertNotExported();
+	if (type_.isNull())
+		type_ = new RowSetType;
+	type_->addRow(rname, rtype);
+	return this;
+}
+
+Nexus *Nexus::exportRowType(const string &name, Onceref<RowType> rtype)
+{
+	assertNotExported();
+	if (name.empty()) {
+		errefAppend(err_, "Can not export a row type with an empty name.", NULL);
+	} else if (rowTypes_.find(name) != rowTypes_.end()) {
+		errefAppend(err_, "Can not export a duplicate row type name '" + name + "'.", NULL);
+	} else if (rtype.isNull()) {
+		errefAppend(err_, "Can not export a NULL row type with name '" + name + "'.", NULL);
+	} else {
+		rowTypes_[name] = rtype;
+	}
+	return this;
+}
+
+Nexus *Nexus::exportTableType(const string &name, Onceref<TableType> tt)
+{
+	assertNotExported();
+	if (name.empty()) {
+		errefAppend(err_, "Can not export a table type with an empty name.", NULL);
+	} else if (tableTypes_.find(name) != tableTypes_.end()) {
+		errefAppend(err_, "Can not export a duplicate table type name '" + name + "'.", NULL);
+	} else if (tt.isNull()) {
+		errefAppend(err_, "Can not export a NULL table type with name '" + name + "'.", NULL);
+	} else {
+		tableTypes_[name] = tt;
+	}
+	return this;
+}
+
+void Nexus::assertNotExported()
+{
+	if (isExported())
+		throw Exception::fTrace("Triceps API violation: attempted to modify an exported nexus '%s/%s'.",
+			tname_.c_str(), name_.c_str());
 }
 
 }; // TRICEPS_NS
--- cpp/app/Nexus.h
+++ cpp/app/Nexus.h
@@ -50,6 +50,13 @@ public:
 	// * export it in the thread, thus initializing it and making it
 	//   visible throughout the app. After that the nexus can't be changed.
 	//
+	// Any encountered errors will be collected and gan be read back with
+	// getErrors(), or will cause an Exception when the nexus is exported.
+	//
+	// After the nexus has been exported, it can not be modified any more
+	// and any such attempts will throw an Exception. The catch is that
+	// this exception may lead to memory leaks, so better just don't do it.
+	//
 	// @param name - name of the nexus, must be unique within the thread.
 	Nexus(const string &name);
 
@@ -57,7 +64,7 @@ public:
 	// This has two implications:
 	// * no queue size limit, no flow control
 	// * this nexus will have a higher reading priority than the direct ones
-	// May be used only until the Nexus is exported.
+	// May be used only until the Nexus is exported or will throw an Exception.
 	// @param on - flag: the direction is reverse
 	// @return - the same Nexus
 	Nexus *setReverse(bool on = true);
@@ -68,7 +75,7 @@ public:
 	// of the readers, chosen essentially at random. This allows
 	// to implement the worker thread pools. A whole transaction
 	// goes to the same reader.
-	// May be used only until the Nexus is exported.
+	// May be used only until the Nexus is exported or will throw an Exception.
 	// @param on - flag: the unicast mode is on
 	// @return - the same Nexus
 	Nexus *setUnicast(bool on = true);
@@ -77,13 +84,13 @@ public:
 	// all its row types copied. This leaves the original type for the
 	// exclusive use of the creating thread. The created type will be
 	// un-initialized and can have more row types added to it.
-	// May be used only until the Nexus is exported.
+	// May be used only until the Nexus is exported or will throw an Exception.
 	// @param rst - the type to set
 	// @return - the same Nexus
 	Nexus *setType(Onceref<RowSetType> rst);
 
 	// Add a row type to the nexus'es queue.
-	// May be used only until the Nexus is exported.
+	// May be used only until the Nexus is exported or will throw an Exception.
 	// @param rtype - row type to add
 	// @return - the same Nexus
 	Nexus *addRow(const string &rname, const_Autoref<RowType>rtype);
@@ -92,7 +99,10 @@ public:
 	// queue, just a row type that can be imported by the other threads.
 	// The type will be copied into the nexus, leaving the original
 	// instance for the exclusive use of the creating thread.
-	// May be used only until the Nexus is exported.
+	// May be called only until the Nexus is exported or will throw an Exception.
+	//
+	// @param name - name of the row type, these are in a separate namespace from
+	//         the row set type of the queue
 	// @param rtype - row type to export
 	// @return - the same Nexus
 	Nexus *exportRowType(const string &name, Onceref<RowType> rtype);
@@ -101,13 +111,19 @@ public:
 	// by the other threads.
 	// The type will be copied into the nexus, leaving the original
 	// instance for the exclusive use of the creating thread.
-	// May be used only until the Nexus is exported.
+	// May be called only until the Nexus is exported or will throw an Exception.
+	//
+	// @param name - name of the table type, these are in a separate namespace from
+	//         the row types
 	// @param tt - table type to export
 	// @return - the same Nexus
 	Nexus *exportTableType(const string &name, Onceref<TableType> tt);
 
 	// Get the collected errors.
-	Erref getErrors() const;
+	Erref getErrors() const
+	{
+		return err_;
+	}
 
 	// Get the name
 	const string &getName() const
@@ -128,10 +144,20 @@ public:
 		return reverse_;
 	}
 
+	// Check whether the nexus is unicast.
+	bool isUnicast() const
+	{
+		return unicast_;
+	}
+
 protected:
+	// Throw an Exception if the nexus has been already exported.
+	void assertNotExported();
+
 	string tname_; // name of the thread that owns this nexus
 	string name_;
 
+	Erref err_; // the collected errors
 	Autoref<RowSetType> type_; // the type of the nexus's main queue
 	RowTypeMap rowTypes_; // the collection of row types
 	TableTypeMap tableTypes_; // the collection of table types
