3a29203fdf66bb3af5c2768de4b4133d4c16f1e5
babkin <babkin@b66b675a-a689-4d1e-b9d0-6c48a4198705>
2013-05-15 03:12:48 +0000

    The first cut at Tql server with multithreading.

--- perl/Triceps/lib/Triceps/X/Tql.pm
+++ perl/Triceps/lib/Triceps/X/Tql.pm
@@ -29,6 +29,7 @@ our $VERSION = 'v1.0.1';
 
 use Carp;
 use Triceps::X::Braced qw(:all);
+use Triceps::X::ThreadedServer qw(printOrShut);
 use Safe;
 
 # There are two ways to create a Tql object:
@@ -39,13 +40,34 @@ use Safe;
 # call initialize().
 #
 # Options:
-# name - name for the object, will be use to derive the sub-object names
-# tables (optional) - reference to an array of tables on which the TQL 
-#   object will allow queries. The presence of this option triggers the
-#   immediate initialization.
-# tableNames (optional) - reference to an array of names, under which the tables
-#   from the option "tables" will be known to TQL. If absent, the table names
-#   will be obtained with getName() for each table.
+# name => $name
+# Name for the object, will be use to derive the sub-object names.
+#
+# trieadOwner => $owner
+# (optional) The TrieadOwner object for the multithreaded operation.
+# This option enables the threaded operation.
+# Default: undef.
+#
+# socketName => $name
+# (optional) Name of the socket in the App. Required when trieadOwner
+# is used.
+# Default: undef.
+#
+# nxprefix => $name
+# (optional) In threaded mode, the prefix for the nexus names that
+# are used for communication between the core of the application
+# and the client threads.
+# Default: "tql".
+#
+# tables => [ @tables ]
+# (optional) Reference to an array of tables on which the TQL 
+# object will allow queries. The presence of this option triggers the
+# immediate initialization.
+#
+# tableNames => [ @tnames ]
+# (optional) Reference to an array of names, under which the tables
+# from the option "tables" will be known to TQL. If absent, the table names
+# will be obtained with getName() for each table.
 sub new # ($class, $optName => $optValue, ...)
 {
 	my $myname = "Triceps::X::Tql";
@@ -56,6 +78,9 @@ sub new # ($class, $optName => $optValue, ...)
 		name => [ undef, \&Triceps::Opt::ck_mandatory ],
 		tables => [ undef, sub { &Triceps::Opt::ck_ref(@_, "ARRAY", "Triceps::Table") } ],
 		tableNames => [ undef, sub { &Triceps::Opt::ck_ref(@_, "ARRAY", "") } ],
+		trieadOwner => [ undef, sub { &Triceps::Opt::ck_ref(@_, "Triceps::TrieadOwner") } ],
+		socketName => [ undef, undef ],
+		nxprefix => [ "tql", undef ],
 	}, @_);
 
 	if (defined $self->{tables}) {
@@ -75,6 +100,9 @@ sub new # ($class, $optName => $optValue, ...)
 		confess "$myname: the option 'tableNames' may not be used without option 'tables'."
 			if (defined $self->{tableNames});
 	}
+
+	confess "$myname: option 'trieadOwner' requires 'socketName'"
+		if (defined $self->{trieadOwner} && !defined $self->{socketName});
 
 	bless $self, $class;
 	return $self;
@@ -123,6 +151,19 @@ sub addTable # ($self, @tables)
 	}
 }
 
+# Processing of the request to dump a table in the main thread,
+# will be chained to every request label in the facet, with the
+# appropriate table argument.
+sub _dumpTable # ($label, $rowop, $self, $table)
+{
+	my ($label, $rop, $self, $table) = @_;
+	my $unit = $label->getUnit();
+	# pass through the client id to the dump
+	$unit->call($self->{beginOut}->adopt($rop)); # XXX not right
+	$table->dumpAll();
+	$self->{faOut}->flushWriter();
+}
+
 # Initialize the object. After that the tables may not be added any more.
 sub initialize # ($self)
 {
@@ -131,25 +172,129 @@ sub initialize # ($self)
 
 	return if ($self->{initialized});
 
-	my %dispatch;
-	my @labels;
-	for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
-		my $name = $self->{tableNames}[$i]; 
-		my $table = $self->{tables}[$i];
+	my $owner = $self->{trieadOwner};
+	if (defined $owner) {
+		my @labels;
+		my @tabtypes;
 
-		confess "$myname: found a duplicate table name '$name', all names are: "
-				. join(", ", @{$self->{tableNames}})
-			if (exists $dispatch{$name});
+		# row type carrying the client id
+		my $rtClient = Triceps::RowType->new(
+			client => "string",
+		) or confess "$!";
 
-		$dispatch{$name} = $table;
-		push @labels, $name, $table->getDumpLabel();
+		# row type for the communication between the client reader
+		# and writer threads, they will pass through the common
+		# nexuses, to synchronized with the data
+		my $rtControl = Triceps::RowType->new(
+			client => "string", # client, for which is this command
+			cmd => "string", # the command
+			id => "string", # the command id
+			arg1 => "string", # the arguments depend on the command
+			arg2 => "string",
+			arg3 => "string",
+		) or confess "$!";
+
+		# build the output side
+		for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
+			my $name = $self->{tableNames}[$i]; 
+			my $table = $self->{tables}[$i];
+
+			push @tabtypes, $name, $table->getType()->copyFundamental();
+			push @labels, "t.out." . $name, $table->getOutputLabel();
+			push @labels, "t.dump." . $name, $table->getDumpLabel();
+		}
+		push @labels, "control", $rtControl;
+
+		$self->{faOut} = $owner->makeNexus(
+			name => $self->{nxprefix} . "out",
+			labels => [ @labels ],
+			tableTypes => [ @tabtypes ],
+			import => "writer",
+		);
+		# XXX use the control labels, since _BEGIN_ will create extra transactions?
+		$self->{beginOut} = $self->{faOut}->getLabel("_BEGIN_");
+		$self->{endOut} = $self->{faOut}->getLabel("_END_");
+
+		# build the input side
+		undef @labels;
+		for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
+			my $name = $self->{tableNames}[$i]; 
+			my $table = $self->{tables}[$i];
+
+			push @labels, "in." . $name, $table->getInputLabel()->getRowType();
+		}
+		push @labels, "control", $rtControl;
+
+		$self->{faIn} = $owner->makeNexus(
+			name => $self->{nxprefix} . "in",
+			labels => [ @labels ],
+			import => "reader",
+		);
+		# tie together the labels
+		for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
+			my $name = $self->{tableNames}[$i]; 
+			my $table = $self->{tables}[$i];
+
+			$self->{faIn}->getLabel("in." . $name)->chain($table->getInputLabel());
+		}
+		# the controll passes through
+		$self->{faIn}->getLabel("control")->chain($self->{faOut}->getLabel("control"));
+
+		# build the dump requests, will be coming from below
+		undef @labels;
+		for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
+			my $name = $self->{tableNames}[$i]; 
+			my $table = $self->{tables}[$i];
+
+			push @labels, "t.rqdump." . $name, $rtClient;
+		}
+		$self->{faRqDump} = $owner->makeNexus(
+			name => $self->{nxprefix} . "rqdump",
+			labels => [ @labels ],
+			reverse => 1, # avoids making a loop, and gives priority
+			import => "reader",
+		);
+		# tie together the labels
+		for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
+			my $name = $self->{tableNames}[$i]; 
+			my $table = $self->{tables}[$i];
+
+			$self->{faRqDump}->getLabel("t.rqdump." . $name)->makeChained(
+				$self->{nxprefix} . "rqdump." . $name, undef, 
+				\&_dumpTable, $self, $table
+			);
+		}
+
+		# start the listener thread
+		Triceps::Triead::start(
+			app => $owner->app()->getName(),
+			thread => $self->{nxprefix} . "Listener",
+			main => \&listenerT,
+			socketName => $self->{socketName},
+			nxprefix => $self->{nxprefix},
+			mainTriead => $owner->getName(),
+		);
+	} else {
+		my %dispatch;
+		my @labels;
+		for (my $i = 0; $i <= $#{$self->{tables}}; $i++) {
+			my $name = $self->{tableNames}[$i]; 
+			my $table = $self->{tables}[$i];
+
+			confess "$myname: found a duplicate table name '$name', all names are: "
+					. join(", ", @{$self->{tableNames}})
+				if (exists $dispatch{$name});
+
+			$dispatch{$name} = $table;
+			push @labels, $name, $table->getDumpLabel();
+		}
+
+		$self->{dispatch} = \%dispatch;
+		$self->{fret} = Triceps::FnReturn->new(
+			name => $self->{name} . ".fret",
+			labels => \@labels,
+		);
 	}
-
-	$self->{dispatch} = \%dispatch;
-	$self->{fret} = Triceps::FnReturn->new(
-		name => $self->{name} . ".fret",
-		labels => \@labels,
-	);
 
 	$self->{initialized} = 1;
 }
@@ -504,6 +649,333 @@ sub query # ($self, $argline)
 		&Triceps::X::SimpleServer::outCurBuf("+ERROR,OP_INSERT,$q: error: $@\n");
 		return
 	}
+}
+
+# Listener for connections.
+# Extra options:
+#
+# socketName => $name
+# The listening socket name in the App.
+#
+# mainTriead => $name
+# Name of the main thread that runs the model and exports the nexuses.
+#
+# nxprefix => $name
+# The prefix for the nexus names that are used for communication between the
+# core of the application and the client threads. Also determines the
+# prefix for the Tql thread names.
+#
+sub listenerT
+{
+	my $opts = {};
+	&Triceps::Opt::parse("listenerT", $opts, {@Triceps::Triead::opts,
+		socketName => [ undef, \&Triceps::Opt::ck_mandatory ],
+		mainTriead => [ undef, \&Triceps::Opt::ck_mandatory ],
+		nxprefix => [ undef, \&Triceps::Opt::ck_mandatory ],
+	}, @_);
+	undef @_;
+	my $owner = $opts->{owner};
+
+	my ($tsock, $sock) = $owner->trackGetSocket($opts->{socketName}, "+<");
+
+	$owner->readyReady();
+
+	Triceps::X::ThreadedServer::listen(
+		owner => $owner,
+		socket => $sock,
+		prefix => $opts->{nxprefix},
+		handler => \&readT,
+		mainTriead => $opts->{mainTriead},
+		nxprefix => $opts->{nxprefix},
+	);
+}
+
+# The socket reading side of the client connection.
+# Parses the data and commands from the client and forwards it through.
+#
+# Extra options:
+#
+# socketName => $name
+# The newly connected client socket name in the App.
+#
+# mainTriead => $name
+# Name of the main thread that runs the model and exports the nexuses.
+#
+# nxprefix => $name
+# The prefix for the nexus names that are used for communication between the
+# core of the application and the client threads. Also determines the
+# prefix for the Tql thread names.
+#
+# The format of incoming user data is: generally CSV,
+# either a data line or command line. The data lines are:
+#
+# d,label,opcode,fields...
+#
+# The "d" is a literal character telling that it's a data entry (essentially
+# like a special command "d"). The field splitting is dumb at the moment and
+# doesn't allow commas inside the data.
+#
+# The command lines are:
+#   
+#   command,id,args...
+#
+# id is the identifier of the command, that will be sent back in the response.
+# The commands are:
+#   d - a data line, see above, has no id
+#   exit - drain and close this connection
+#   shutdown - drain and shut down the whole server
+#   confirm - will send back a confirmation when the data up to this point
+#     has been processed in the core logic, no arguments
+#   drain - will send back a confirmation when the data up to this point
+#     has been fully processed and drained
+#   subscribe - subscribe to a label or table, just gets the new updates,
+#     without the initial contents; args: label or table name
+#   dump - dump a table, will send the confirmation after the end of data;
+#     args: table name
+#   dumpsub - dump a table and then continue with subscription to updates, 
+#     will send the confirmation after the end of dumped data;
+#     args: table name
+#   query - do an one-time query of the table contents, will send confirmation
+#     after the end of data; query text is braced;
+#     args: name of the query, text of the query
+#     (NOT SUPPORTED YET)
+#   querysub - do an one-time query of the table contents, followed by the
+#     subscription to running updates, will send confirmation
+#     after the end of data; query text is braced;
+#     args: name of the query, text of the query
+sub readT
+{
+	my $opts = {};
+	&Triceps::Opt::parse("chatSockReadT", $opts, {@Triceps::Triead::opts,
+		socketName => [ undef, \&Triceps::Opt::ck_mandatory ],
+		mainTriead => [ undef, \&Triceps::Opt::ck_mandatory ],
+		nxprefix => [ undef, \&Triceps::Opt::ck_mandatory ],
+	}, @_);
+	undef @_; # avoids a leak in threads module
+	my $owner = $opts->{owner};
+	my $app = $owner->app();
+	my $unit = $owner->unit();
+	my $tname = $opts->{thread};
+	my $fragment = $opts->{fragment}; # this is the client name
+
+	# only dup the socket, the writer thread will consume it
+	my ($tsock, $sock) = $owner->trackDupSocket($opts->{socketName}, "<");
+
+	# messages will be sent here
+	my $faIn = $owner->importNexus(
+		from => $opts->{mainTriead} . "/" . $opts->{nxprefix} . "in",
+		import => "writer",
+	);
+
+	Triceps::Triead::start(
+		app => $opts->{app},
+		thread => "$tname.rd",
+		fragment => $opts->{fragment},
+		main => \&writeT,
+		socketName => $opts->{socketName},
+		mainTriead => $opts->{mainTriead},
+		nxprefix => $opts->{nxprefix},
+	);
+
+	$owner->readyReady();
+
+	my $lbCtl = $faIn->getLabel("control");
+
+	my %passcmd = ( # these commands are treated all the same, by passing to the writeT
+		confirm => 1,
+		drain => 1,
+		subscribe => 1,
+		dump => 1,
+		dumpsub => 1,
+		query => 1,
+		querysub => 1,
+	);
+
+	while(<$sock>) {
+		s/[\r\n]+$//;
+		my @data = split(/,/, $_);
+		if ($data[0] eq "exit") {
+			last; # a special case, handle in this thread
+		} elsif ($data[0] eq "shutdown") {
+			my $drain = Triceps::AutoDrain::makeExclusive($owner);
+			# the writers will accept the shutdown command from any client
+			$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => "shutdown");
+			$owner->flushWriters();
+			$drain->wait();
+			eval {$app->shutdown();};
+			last;
+		} elsif ($data[0] eq "d") {
+			shift @data;
+			my $dest = shift @data;
+			my $lb = eval {
+				$faIn->getLabel("in." . $dest);
+			};
+			if (!defined $lb) {
+				$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => "error",
+					arg1 => "Bad label in command 'd': '$dest'", arg2 => "bad_dest",
+					arg3 => $dest);
+			} else {
+				if (!eval {
+					$unit->makeArrayCall($lb, @data);
+				}) {
+					chomp $@;
+					$@ =~ s/\n/\\n/g; # no real newlines in the output
+					$@ =~ s/,/;/g; # no confusing commas in the output
+					$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => "error",
+						arg1 => "Bad data in command 'd' $dest: $@", arg2 => "bad_data",
+						arg3 => join(',', @data));
+				}
+			}
+		} elsif (exists $passcmd{$data[0]}) {
+			my @data = split(/,/, $_, 4); # the 4th argument will be the query, left unsplit
+			$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => $data[0],
+				id => $data[1], arg1 => $data[2], arg2 => $data[3]);
+		} elsif ($data[0] eq "drain") {
+			Triceps::AutoDrain::makeShared($owner);
+			$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => "drain", id => $data[1]);
+		} else {
+			$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => "error", id => $data[1],
+				arg1 => "Bad command: '" . $data[0] . "'", arg2 => "bad_command",
+				arg3 => $data[0]);
+		}
+		$owner->flushWriters();
+	}
+
+	{
+		# let the data drain through
+		my $drain = Triceps::AutoDrain::makeExclusive($owner);
+
+		# send the notification - can do it because the drain is excluding itself
+		$unit->makeHashCall($lbCtl, "OP_INSERT", client => $fragment, cmd => "exit");
+		$owner->flushWriters();
+
+		$drain->wait(); # wait for the notification to drain
+
+		$app->shutdownFragment($opts->{fragment});
+	}
+
+	$tsock->close(); # not strictly necessary
+}
+
+# The actual execution of the commands and sending of the results.
+#
+# Extra options:
+#
+# socketName => $name
+# The newly connected client socket name in the App.
+#
+# mainTriead => $name
+# Name of the main thread that runs the model and exports the nexuses.
+#
+# nxprefix => $name
+# The prefix for the nexus names that are used for communication between the
+# core of the application and the client threads. Also determines the
+# prefix for the Tql thread names.
+#
+# Besides the "real" user-initiated commands, the following commands are recognized:
+# 
+# exit - send a notification of the client exit
+# shutdown - send a notification of the server shutdown
+# error - send a notification of an error in the user data
+#
+# The output is either a data line or command confirmation line. The data lines are:
+#
+# d,label,opcode,fields...
+#
+# The command confirmation lines are:
+#
+# command,id,[label]
+# error,id,user_text,type,value....
+#
+# The command confirmation name is generally the same as command name. Whether the
+# label will be present in it, depends on whether the label is present in the 
+# original command.
+#
+# The error report may have the real or empty id, depending on whether it was
+# present in the original command. The error report contains the user-readable
+# text, the error type, and the value that triggered the error. The value
+# may be multi-field. The user-readable text is guaranteed to have any commas
+# in it replaced with ';' and any line feeds with '\n'.
+#
+sub writeT
+{
+	my $opts = {};
+	&Triceps::Opt::parse("chatSockWriteT", $opts, {@Triceps::Triead::opts,
+		socketName => [ undef, \&Triceps::Opt::ck_mandatory ],
+		mainTriead => [ undef, \&Triceps::Opt::ck_mandatory ],
+		nxprefix => [ undef, \&Triceps::Opt::ck_mandatory ],
+	}, @_);
+	undef @_;
+	my $owner = $opts->{owner};
+	my $app = $owner->app();
+	my $tname = $opts->{thread};
+	my $fragment = $opts->{fragment}; # this is the client name
+
+	my ($tsock, $sock) = $owner->trackGetSocket($opts->{socketName}, ">");
+
+	# incoming data and control commands
+	my $faOut = $owner->importNexus(
+		from => $opts->{mainTriead} . "/" . $opts->{nxprefix} . "out",
+		import => "reader",
+	);
+
+	# the table dump requests to the core of the App
+	my $faRqDump = $owner->importNexus(
+		from => $opts->{mainTriead} . "/" . $opts->{nxprefix} . "rqdump",
+		import => "writer",
+	);
+
+	# the commands that get handled by passing them through
+	my %passcmds = (
+		exit => 1,
+		shutdown => 1,
+		confirm => 1,
+		drain => 1,
+		error => 1,
+	);
+
+	my %subscr; # the currently active subscriptions
+
+	$faOut->getLabel("control")->makeChained("lbCtl", undef, sub {
+		my $row = $_[1]->getRow();
+		my ($client, $cmd, $id, @args) = $row->toArray();
+
+		if ($client eq $fragment || $cmd eq "shutdown") {
+			if (exists $passcmds{$cmd}) {
+				printOrShut($app, $fragment, $sock, join(',', $cmd, $id, @args), "\n");
+			} elsif ($cmd eq "subscribe") {
+				if (!exists $subscr{$args[0]}) {
+					my $lbsrc = eval {
+						$faOut->getLabel("t.out." . $args[0]);
+					};
+					if (!$lbsrc) {
+						printOrShut($app, $fragment, $sock,
+							"error,$id,Bad label: '$cmd',bad_label," . $args[0] . "\n");
+						return;
+					}
+					$lbsrc->makeChained("lbOut." . $args[0], undef, sub {
+						printOrShut($app, $fragment, $sock, 
+							"d,", $_[2], ",", Triceps::opcodeString($_[1]->getOpcode()), ",",
+							join(",", $_[1]->getRow()->toArray()), "\n");
+					}, $args[0]);
+					$subscr{$args[0]} = 1;
+				}
+				printOrShut($app, $fragment, $sock, join(',', $cmd, $id, $args[0]), "\n");
+			} else {
+				printOrShut($app, $fragment, $sock,
+					"error,$id,Bad command: '$cmd',bad_command,$cmd\n");
+			}
+		}
+	});
+
+	$owner->readyReady();
+
+	# the first prompt
+	printOrShut($app, $fragment, $sock, "ready\n");
+
+	$owner->mainLoop();
+
+	$tsock->close(); # not strictly necessary
 }
 
 1;
---
+++ perl/Triceps/t/xTqlMt.t
@@ -0,0 +1,149 @@
+#
+# (C) Copyright 2011-2013 Sergey A. Babkin.
+# This file is a part of Triceps.
+# See the file COPYRIGHT for the copyright notice and license information
+#
+# The example of queries in TQL (Triceps/Trivial Query Language)
+# with multithreading.
+
+#########################
+
+use ExtUtils::testlib;
+
+use Test;
+BEGIN { plan tests => 1 };
+use Triceps;
+use Triceps::X::TestFeed qw(:all);
+use Triceps::X::Tql;
+use Triceps::X::ThreadedServer;
+use Triceps::X::ThreadedClient;
+use Carp;
+ok(2); # If we made it this far, we're ok.
+
+use strict;
+
+#########################
+
+# Insert your test code below, the Test::More module is use()ed here so read
+# its man page ( perldoc Test::More ) for help writing this test script.
+
+#########################
+# The simple App example.
+
+package App1;
+use Carp;
+
+sub appCoreT # (@opts)
+{
+	my $opts = {};
+	&Triceps::Opt::parse("appCoreT", $opts, {@Triceps::Triead::opts,
+		socketName => [ undef, \&Triceps::Opt::ck_mandatory ],
+	}, @_);
+	undef @_; # avoids a leak in threads module
+	my $owner = $opts->{owner};
+	my $app = $owner->app();
+	my $unit = $owner->unit();
+
+	# build the core logic
+
+	my $rtTrade = Triceps::RowType->new(
+		id => "int32", # trade unique id
+		symbol => "string", # symbol traded
+		price => "float64",
+		size => "float64", # number of shares traded
+	) or confess "$!";
+
+	my $ttWindow = Triceps::TableType->new($rtTrade)
+		->addSubIndex("bySymbol", 
+			Triceps::SimpleOrderedIndex->new(symbol => "ASC")
+				->addSubIndex("last2",
+					Triceps::IndexType->newFifo(limit => 2)
+				)
+		)
+		or confess "$!";
+	$ttWindow->initialize() or confess "$!";
+
+	# Represents the static information about a company.
+	my $rtSymbol = Triceps::RowType->new(
+		symbol => "string", # symbol name
+		name => "string", # the official company name
+		eps => "float64", # last quarter earnings per share
+	) or confess "$!";
+
+	my $ttSymbol = Triceps::TableType->new($rtSymbol)
+		->addSubIndex("bySymbol", 
+			Triceps::IndexType->newHashed(key => [ "symbol" ])
+		)
+		or confess "$!";
+	$ttSymbol->initialize() or confess "$!";
+
+	my $tWindow = $unit->makeTable($ttWindow, "EM_CALL", "tWindow")
+		or confess "$!";
+	my $tSymbol = $unit->makeTable($ttSymbol, "EM_CALL", "tSymbol")
+		or confess "$!";
+
+	# $tSymbol->getOutputLabel()->makeChained("dbgSymbol", undef, sub {
+		# print "XXX ", $_[1]->printP(), "\n";
+	# });
+
+	# export the endpoints for TQL (it starts the listener)
+	my $tql = Triceps::X::Tql->new(
+		name => "tql",
+		trieadOwner => $owner,
+		socketName => $opts->{socketName},
+		tables => [
+			$tWindow,
+			$tSymbol,
+		],
+		tableNames => [
+			"window",
+			"symbol",
+		],
+	);
+
+	$owner->readyReady();
+
+	$owner->mainLoop();
+}
+
+{
+	my ($port, $thread) = Triceps::X::ThreadedServer::startServer(
+			app => "appTql",
+			main => \&appCoreT,
+			port => 0,
+			fork => -1, # create a thread, not a process
+	);
+
+	Triceps::App::build "client", sub {
+		my $appname = $Triceps::App::name;
+		my $owner = $Triceps::App::global;
+
+		# give the port in startClient
+		my $client = Triceps::X::ThreadedClient->new(
+			owner => $owner,
+			debug => 1,
+		);
+
+		$owner->readyReady();
+
+		$client->startClient("c1", $port);
+		$client->expect("c1", 'ready');
+
+		$client->send("c1", "subscribe,s1,symbol\n");
+		$client->expect("c1", 'subscribe,s1');
+
+		$client->send("c1", "d,symbol,OP_INSERT,ABC,ABC Corp,1.0\n");
+		$client->expect("c1", 'd,symbol,OP_INSERT,ABC,ABC Corp,1$');
+
+		$client->send("c1", "shutdown\n");
+		$client->expect("c1", '__EOF__');
+
+		print $client->protocol();
+		#ok($client->protocol(), '');
+	};
+
+	$thread->join();
+}
+
+# check that everything completed and not died
+::ok(1);
